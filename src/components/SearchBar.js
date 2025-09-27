a83c2188dd76a1960c1a2620e12db87accaa92fd
thesarikonda <140501081+thesarikonda@users.noreply.github.com>
2025-09-27 20:21:55 +0530

    commit8

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,35 +1,22 @@
 import React from "react";
-import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
-import logo from "../assets/logo.png";
-import "./Navbar.css";
+import styles from "./Navbar.css";
+import Logo from "../assets/logo.png";
+import SearchBar from "./SearchBar.js";
+import Button from "./Button.js";
 
 const Navbar = () => {
-  return (
-    <Box
-      component="nav"
-      className="navbar"
-      display="flex"
-      alignItems="center"
-      justifyContent="space-between"
-      px={4}
-      height="75px"
-      bgcolor="#1DB954"
-    >
-      {/* Logo */}
-      <img src={logo} alt="logo" className="navbar-logo" />
 
-      {/* Search Bar */}
-      <input
-        type="text"
-        placeholder="search a song"
-        className="navbar-search"
-      />
+    return (
+        <>
+            <nav className={styles.navbar}>
+                <Logo />
+                <SearchBar search={"Search a song of your choice"} />
+                <Button children={"Give Feedback"} />
+            </nav>
 
-      {/* Feedback Button */}
-      <button className="navbar-btn">Give Feedback</button>
-    </Box>
-  );
-};
+        </>
+
+    );
+}
 
 export default Navbar;
